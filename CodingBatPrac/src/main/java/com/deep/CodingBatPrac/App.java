4247c3dc0d0eecd60a14d4cd91d5520dd8550b9f
deepanraj_b <deepanraj_b@LT0220.solartistech.local>
2025-07-10 13:52:59 +0530

    New Commit

---
+++ CodingBatPrac/src/main/java/com/deep/CodingBatPrac/App.java
@@ -0,0 +1,13 @@
+package com.deep.CodingBatPrac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
